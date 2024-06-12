77cdb57f7568b3759e1d90b0bb7e2c523e6626f3
yuvalitzhaki <yyuval1971@gmail.com>
2024-06-12 15:13:15 +0300

    small change for summery component

--- src/components/SummarySection.vue
+++ src/components/SummarySection.vue
@@ -1,15 +1,29 @@
 <template>
     <div class="summary-section">
       <h2>Profile</h2>
-      <p>{{ summery }}</p>
+      <p>{{ profile }}</p>
     </div>
   </template>
   
   <script setup>
-  import { ref } from 'vue';
-  const summery = ref(
-    'Experienced software engineer with a strong foundation in computer science fundamentals and a track record of delivering high-quality solutions, Proven ability to collaborate eectively in cross-functional teams and thrive in fast-paced environments, Skilled in problem-solving and equipped to tackle diverse challenges with creativity and resilience.'
-);
+import { ref, onMounted } from 'vue';
+    import axios from 'axios';
+
+    const profile = ref([]);
+
+    onMounted(async () => {
+    try {
+        const response = await axios.get('http://localhost:5001/api/resumes');
+        profile.value = response.data.profile;
+    } catch (error) {
+        console.error('Error fetching resume data:', error);
+    }
+    });
+
+//   import { ref } from 'vue';
+//   const summery = ref(
+//     'Experienced software engineer with a strong foundation in computer science fundamentals and a track record of delivering high-quality solutions, Proven ability to collaborate eectively in cross-functional teams and thrive in fast-paced environments, Skilled in problem-solving and equipped to tackle diverse challenges with creativity and resilience.'
+// );
   </script>
   
   <style scoped>
