88c9d84e4c09be24d0c57a59d6e50911a2ec6aba
yuvalitzhaki <yyuval1971@gmail.com>
2024-06-21 18:24:56 +0300

    first form refactor

--- src/components/ResumeForm.vue
+++ src/components/ResumeForm.vue
@@ -1,142 +1,269 @@
 <template>
-  <div class="page-container">
-    <form @submit.prevent="handleSubmit" class="form-container">
-      <div class="form-group">
-        <label for="name">Name:</label>
-        <input id="name" v-model="formData.profile.name" type="text" required />
-      </div>
-      <div class="form-group">
-        <label for="title">Title:</label>
-        <input id="title" v-model="formData.profile.title" type="text" required />
-      </div>
-      <fieldset class="form-group">
-        <legend>Contact</legend>
-        <div class="contact-group">
-          <div class="form-group">
-            <label for="contact-email">Email:</label>
-            <input id="contact-email" v-model="formData.contact.email" type="email" required />
-          </div>
-          <div class="form-group">
-            <label for="contact-phone">Phone:</label>
-            <input id="contact-phone" v-model="formData.contact.phone" type="text" required />
-          </div>
-          <div class="form-group">
-            <label for="contact-linkedin">LinkedIn:</label>
-            <input id="contact-linkedin" v-model="formData.contact.linkedin" type="text" required />
-          </div>
-          <div class="form-group">
-            <label for="contact-github">GitHub:</label>
-            <input id="contact-github" v-model="formData.contact.github" type="text" required />
-          </div>
-        </div>
-      </fieldset>
-      <div class="form-group">
-        <label for="tech_skills">Tech Skills:</label>
-        <v-select 
-          v-model="formData.tech_skills" 
-          :options="techSkillsOptions" 
-          multiple 
-          label="label" 
-          placeholder="Select tech skills" 
-          :reduce="skill => skill.value"
-          style="width: 100%; padding: 10px; border: 1px solid #ccc; border-radius: 5px;"
-        />
-      </div>
-      <div class="form-group">
-      <label for="languages">Languages:</label>
-      <div v-for="(lang, langIndex) in formData.languages" :key="langIndex" class="language-group">
-        <v-select 
-          v-model="lang.value" 
-          :options="languagesOptions.map(l => l.value)" 
-          label="label" 
-          placeholder="Select language" 
-          @input="updateLanguageLevel(langIndex)"
-          class="language-select"
-        />
-        <v-select 
-          v-model="lang.level" 
-          :options="getLanguageLevels(lang.value)" 
-          label="label" 
-          placeholder="Select proficiency level" 
-          class="level-select"
-        />
-    </div>
-    <button type="button" @click="addLanguage">Add Language</button>
-</div>
+  <v-container class="page-container">
+    <v-form  @submit.prevent="handleSubmit" ref="form">
+      <v-card class="form-container">
+        <v-card-title>
+          <span class="headline">Profile Form</span>
+        </v-card-title>
 
-      <fieldset class="form-group">
-        <legend>Education</legend>
-        <div v-for="(edu, index) in formData.education" :key="index" class="education-group">
-          <div class="form-group">
-            <label :for="'institution-' + index">Institution:</label>
-            <input :id="'institution-' + index" v-model="edu.institution" type="text" required />
+        <v-card-text>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="formData.profile.name"
+                label="Name"
+                required
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12">
+              <v-text-field
+                v-model="formData.profile.title"
+                label="Title"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-divider></v-divider>
+
+          <v-card-subtitle>Contact</v-card-subtitle>
+
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="formData.contact.email"
+                label="Email"
+                type="email"
+                required
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12">
+              <v-text-field
+                v-model="formData.contact.phone"
+                label="Phone"
+                type="tel"
+                required
+                :rules="[phoneRule]"
+                placeholder="+14155552671"
+              ></v-text-field>
+              <v-alert v-if="phoneError" type="error">{{ phoneError }}</v-alert>
+            </v-col>
+
+            <v-col cols="12">
+              <v-text-field
+                v-model="formData.contact.linkedin"
+                label="LinkedIn"
+                required
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12">
+              <v-text-field
+                v-model="formData.contact.github"
+                label="GitHub"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-divider></v-divider>
+
+          <v-card-subtitle>Tech Skills</v-card-subtitle>
+
+          <v-row>
+            <v-col cols="12">
+              <v-autocomplete
+                v-model="formData.tech_skills"
+                :items="techSkillsOptions"
+                item-title="label"
+                item-value="value"
+                label="Tech Skills"
+                multiple
+                chips
+                required
+              ></v-autocomplete>
+            </v-col>
+          </v-row>
+
+          <v-divider></v-divider>
+
+          <v-card-subtitle>Languages</v-card-subtitle>
+
+          <v-row v-for="(lang, langIndex) in formData.languages" :key="langIndex">
+            <v-col cols="6">
+              <v-autocomplete
+                v-model="lang.value"
+                :items="languagesOptions.map(l => l.value)"
+                label="Language"
+                @input="updateLanguageLevel(langIndex)"
+                required
+              ></v-autocomplete>
+            </v-col>
+            <v-col cols="6">
+              <v-autocomplete
+                v-model="lang.level"
+                :items="getLanguageLevels(lang.value)"
+                label="Proficiency Level"
+                required
+              ></v-autocomplete>
+            </v-col>
+          </v-row>
+          <div class="mb-4">
+            <v-btn text @click="addLanguage">Add Language</v-btn>
           </div>
-          <div class="form-group">
-            <label :for="'degree-' + index">Degree:</label>
-            <input :id="'degree-' + index" v-model="edu.degree" type="text" required />
+          <v-divider></v-divider>
+
+          <v-card-subtitle>Education</v-card-subtitle>
+
+          <v-row v-for="(edu, index) in formData.education" :key="index">
+            <v-col cols="12">
+              <v-text-field
+                v-model="edu.institution"
+                :label="'Institution ' + (index + 1)"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                v-model="edu.degree"
+                :label="'Degree ' + (index + 1)"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="edu.startDate"
+                :label="'Start Date ' + (index + 1)"
+                outlined
+                readonly
+                @click="openDatePicker(index, 'start')"
+                required
+              ></v-text-field>
+              <!-- Date Picker -->
+              <v-date-picker
+                v-if="edu.showStartDatePicker"
+                v-model="edu.startDate"
+                :label="'Start Date ' + (index + 1)"
+                @input="closeDatePicker(index, 'start')"
+                ref="datePickerStart"
+              ></v-date-picker>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="edu.endDate"
+                :label="'End Date ' + (index + 1)"
+                outlined
+                readonly
+                @click="openDatePicker(index, 'end')"
+                required
+              ></v-text-field>
+              <!-- Date Picker -->
+              <v-date-picker
+                v-if="edu.showEndDatePicker"
+                v-model="edu.endDate"
+                :label="'End Date ' + (index + 1)"
+                @input="closeDatePicker(index, 'end')"
+                ref="datePickerEnd"
+              ></v-date-picker>
+            </v-col>
+          </v-row>
+          <div class="mb-4">
+            <v-btn text @click="addEducation">Add Education</v-btn>
+            <v-btn text @click="removeEducation(index)">Remove</v-btn>
           </div>
-          <div class="form-group">
-            <label :for="'startDate-' + index">Start Date:</label>
-            <input :id="'startDate-' + index" v-model="edu.startDate" type="text" required />
+          <v-divider></v-divider>
+
+          <!-- Experience and other sections omitted for brevity -->
+
+        <!-- </v-card-text>
+
+        
+      </v-card> -->
+
+          <v-divider></v-divider>
+
+          <v-card-subtitle>Experience</v-card-subtitle>
+
+          <v-row v-for="(exp, index) in formData.experience" :key="index">
+            <v-col cols="12">
+              <v-text-field
+                v-model="exp.title"
+                :label="'Title ' + (index + 1)"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                v-model="exp.company"
+                :label="'Company ' + (index + 1)"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="exp.startDate"
+                :label="'Start Date ' + (index + 1)"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="exp.endDate"
+                :label="'End Date ' + (index + 1)"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-textarea
+                v-model="exp.duties"
+                :label="'Duties ' + (index + 1)"
+                required
+              ></v-textarea>
+            </v-col>
+          </v-row>
+          <div class="mb-4">
+            <v-btn text @click="addExperience">Add Experience</v-btn>
+            <v-btn text @click="removeExperience(index)">Remove</v-btn>
           </div>
-          <div class="form-group">
-            <label :for="'endDate-' + index">End Date:</label>
-            <input :id="'endDate-' + index" v-model="edu.endDate" type="text" required />
-          </div>
-          <button type="button" @click="removeEducation(index)">Remove</button>
-        </div>
-        <button type="button" @click="addEducation">Add Education</button>
-      </fieldset>
-      <fieldset class="form-group">
-        <legend>Experience</legend>
-        <div v-for="(exp, index) in formData.experience" :key="index" class="experience-group">
-          <div class="form-group">
-            <label :for="'exp-title-' + index">Title:</label>
-            <input :id="'exp-title-' + index" v-model="exp.title" type="text" required />
-          </div>
-          <div class="form-group">
-            <label :for="'exp-company-' + index">Company:</label>
-            <input :id="'exp-company-' + index" v-model="exp.company" type="text" required />
-          </div>
-          <div class="form-group">
-            <label :for="'exp-startDate-' + index">Start Date:</label>
-            <input :id="'exp-startDate-' + index" v-model="exp.startDate" type="text" required />
-          </div>
-          <div class="form-group">
-            <label :for="'exp-endDate-' + index">End Date:</label>
-            <input :id="'exp-endDate-' + index" v-model="exp.endDate" type="text" required />
-          </div>
-          <div class="form-group">
-            <label :for="'exp-duties-' + index">Duties:</label>
-            <textarea :id="'exp-duties-' + index" v-model="exp.duties" required></textarea>
-          </div>
-          <button type="button" @click="removeExperience(index)">Remove</button>
-        </div>
-        <button type="button" @click="addExperience">Add Experience</button>
-      </fieldset>
-      <div class="form-group">
-        <label for="profile">Profile:</label>
-        <input id="profile" v-model="formData.summary" type="text" required />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  </div>
+          <v-divider></v-divider>
+
+          <v-card-subtitle>Profile</v-card-subtitle>
+
+          <v-row>
+            <v-col cols="12">
+              <v-textarea
+                v-model="formData.summary"
+                label="Profile"
+                required
+              ></v-textarea>
+            </v-col>
+          </v-row>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn type="submit" color="primary">Submit</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-form>
+  </v-container>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import { defineEmits } from 'vue';
-import vSelect from 'vue-select';
-import 'vue-select/dist/vue-select.css';
 import { useRouter } from 'vue-router';
-import {techSkillsOptions, languagesOptions} from '../data/data.json'
+import { techSkillsOptions, languagesOptions } from '../data/data.json';
 
 const router = useRouter();
-
 const emit = defineEmits(['submit']);
+const form = ref(null);
+const showModal = ref(false);
 const formData = ref({
-  name: '',
-  email: '',
+  profile: {
+    name: '',
+    title: ''
+  },
   contact: {
     email: '',
     phone: '',
@@ -145,34 +272,33 @@ const formData = ref({
   },
   tech_skills: [],
   languages: [{ value: '', level: '' }],
-  education: [
-    {
-      institution: '',
-      degree: '',
-      startDate: '',
-      endDate: ''
-    }
-  ],
-  experience: [
-    {
-      title: '',
-      company: '',
-      startDate: '',
-      endDate: '',
-      duties: ''
-    }
-  ],
-  profile: {
-    name: '',
+  education: [{
+    institution: '',
+    degree: '',
+    startDate: null,
+    endDate: null,
+    showStartDatePicker: false,
+    showEndDatePicker: false
+  }],
+  experience: [{
     title: '',
-  },
-  summary: '',
+    company: '',
+    startDate: '',
+    endDate: '',
+    duties: ''
+  }],
+  summary: ''
 });
 
+const phonePattern = '^\\+?[1-9]\\d{1,14}$';
+const phoneError = ref('');
+
 const handleSubmit = () => {
-  console.log('Form submitted:', formData.value);
-  router.push({ name: 'ResumePreview' });
-  emit('submit', formData.value);
+  if (form.value.validate()) {
+    console.log('Form submitted:', formData.value);
+    router.push({ name: 'ResumePreview' });
+    emit('submit', formData.value);
+  }
 };
 
 const addLanguage = () => {
@@ -180,12 +306,11 @@ const addLanguage = () => {
 };
 
 const updateLanguageLevel = (langIndex) => {
-  formData.value.languages[langIndex].level = '';  // Reset the level when the language is changed
-};
+  formData.value.languages[langIndex].level = ''; 
+} ;
 
 const getLanguageLevels = (language) => {
   const lang = languagesOptions.find(l => l.value === language);
-  console.log('lang', lang)
   return lang ? lang.levels : [];
 };
 
@@ -193,8 +318,10 @@ const addEducation = () => {
   formData.value.education.push({
     institution: '',
     degree: '',
-    startDate: '',
-    endDate: ''
+    startDate: null,
+    endDate: null,
+    showStartDatePicker: false,
+    showEndDatePicker: false
   });
 };
 
@@ -202,6 +329,21 @@ const removeEducation = (index) => {
   formData.value.education.splice(index, 1);
 };
 
+const openDatePicker = (index, type) => {
+  if (type === 'start') {
+    formData.value.education[index].showStartDatePicker = true;
+  } else if (type === 'end') {
+    formData.value.education[index].showEndDatePicker = true;
+  }
+};
+
+const closeDatePicker = (index, type) => {
+  if (type === 'start') {
+    formData.value.education[index].showStartDatePicker = false;
+  } else if (type === 'end') {
+    formData.value.education[index].showEndDatePicker = false;
+  }
+};
 const addExperience = () => {
   formData.value.experience.push({
     title: '',
@@ -215,92 +357,49 @@ const addExperience = () => {
 const removeExperience = (index) => {
   formData.value.experience.splice(index, 1);
 };
+const phoneRule = (value) => {
+  const regex = new RegExp(phonePattern);
+  return regex.test(value) || 'Invalid phone number format';
+};
 </script>
-
 
 <style scoped>
 .page-container {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 240vh;
+  min-height: 100vh;
   background-color: #f5f5f5;
 }
 
 .form-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   width: 100%;
-  max-width: 600px;
+  max-width: 800px;
   padding: 20px;
-  background-color: white;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
-.form-group {
-  width: 100%;
-  margin-bottom: 15px;
-}
-
-.form-group label {
-  display: block;
-  margin-bottom: 5px;
-  font-weight: bold;
-}
-
-.form-group input, .form-group textarea {
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
-
-.contact-group {
+.language-group,
+.education-group,
+.experience-group {
   display: flex;
-  flex-direction: column;
-  gap: 15px;
-}
-
-.language-group {
-  display: flex;
-  align-items: center;
+  flex-wrap: wrap;
   gap: 10px;
 }
 
-.language-select, .level-select {
+.language-select,
+.level-select {
   flex: 1;
 }
 
-.education-group, .experience-group {
-  margin-bottom: 15px;
+.v-card-title,
+.v-card-subtitle {
+  text-align: center;
 }
 
-button {
-  padding: 10px 20px;
-  border: none;
-  background-color: #00796b;
-  color: white;
-  border-radius: 5px;
-  cursor: pointer;
+.v-btn {
+  margin-top: 15px;
 }
-
-button:hover {
-  background-color: #005f56;
-}
-
-fieldset {
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  margin-bottom: 15px;
-}
-
-legend {
-  padding: 0 10px;
-  font-weight: bold;
+.mb-4 {
+  margin-bottom: 16px;
 }
 </style>
-
