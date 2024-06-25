28a4ba63d2bfef76faebc0a59e356274406a3b04
yuvalitzhaki <yyuval1971@gmail.com>
2024-06-25 12:01:02 +0300

    Add file the presents the resume list

---
+++ src/views/MyResume.vue
@@ -0,0 +1,17 @@
+<!-- src/views/ResumePreview.vue -->
+<template>
+    <div>
+      <h1>My Resumes</h1>
+      <div><ResumeList /></div>
+    </div>
+  </template>
+  
+  <script setup>
+  import ResumeList from '../components/ResumeList.vue'
+  
+  </script>
+  
+  <style scoped>
+  /* Component-specific styles */
+  </style>
+  
